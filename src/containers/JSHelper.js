e0bf02af878f0453c7bfed21ab44ea0e4d823af8
Colin McNamara <coolinmc6@gmail.com>
2018-03-14 09:35:38 -0400

    Syntax highlighting and library update

--- src/containers/JSHelper.js
+++ src/containers/JSHelper.js
@@ -3,17 +3,30 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { updateSearch, fetchLibrary } from '../actions/index';
 
+import '../css/prism.css';
+
+// import Prism from 'prismjs';
+
 class JSHelper extends Component {
 	componentDidMount() {
 		this.props.fetchLibrary();
 	}
 
+	createMarkup(html) {
+	  return {__html: html};
+	  // const code = "const data = { data: ['1,2,3,4,5] }";
+	  // const html = Prism.highlight(code, Prism.languages.javascript);
+	  // <div dangerouslySetInnerHTML={this.createMarkup(html)} />
+	}
+
 	renderSearch() {
 		return this.props.javascript.list.map((item) =>  {
+			
 			return (
 				<div key={item.id}>
 					<h3>{item.term}</h3>
 					<p>{item.definition}</p>
+					
 				</div>
 			)
 		});
